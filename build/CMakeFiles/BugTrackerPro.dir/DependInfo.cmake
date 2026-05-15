d5dcaee5e13fbc0c1a4623bb55dab4a77aee931f
Riichxiong99 <richardxiong0224@hotmail.ca>
2026-05-14 22:34:46 -0700

    bug fix

---
+++ build/CMakeFiles/BugTrackerPro.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/Bug.cpp" "CMakeFiles/BugTrackerPro.dir/Src/Bug.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/Bug.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/Customer.cpp" "CMakeFiles/BugTrackerPro.dir/Src/Customer.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/Customer.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/Employee.cpp" "CMakeFiles/BugTrackerPro.dir/Src/Employee.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/Employee.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/Features.cpp" "CMakeFiles/BugTrackerPro.dir/Src/Features.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/Features.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/Products.cpp" "CMakeFiles/BugTrackerPro.dir/Src/Products.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/Products.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/UI.cpp" "CMakeFiles/BugTrackerPro.dir/Src/UI.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/UI.cpp.o.d"
+  "/home/icebankyou/Documents/BugTrackingProgram/Src/main.cpp" "CMakeFiles/BugTrackerPro.dir/Src/main.cpp.o" "gcc" "CMakeFiles/BugTrackerPro.dir/Src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BugTrackerPro.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "BugTrackerPro"
+  "BugTrackerPro.pdb"
+  "CMakeFiles/BugTrackerPro.dir/Src/Bug.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/Bug.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/Customer.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/Customer.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/Employee.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/Employee.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/Features.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/Features.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/Products.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/Products.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/UI.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/UI.cpp.o.d"
+  "CMakeFiles/BugTrackerPro.dir/Src/main.cpp.o"
+  "CMakeFiles/BugTrackerPro.dir/Src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BugTrackerPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
